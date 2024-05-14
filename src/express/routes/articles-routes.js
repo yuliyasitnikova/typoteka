fedd7fadc27ece5ac92e5d14ec2580bf46c97a67
Юлия Ситникова <yu.sitnikova@inbox.ru>
2024-05-15 00:50:49 +0500

    Обновляет обработчики маршрутов

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -4,9 +4,9 @@ const {Router} = require(`express`);
 
 const articlesRoutes = new Router();
 
-articlesRoutes.get(`/:id`, (req, res) => res.send(`/articles/:id`));
-articlesRoutes.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-articlesRoutes.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
+articlesRoutes.get(`/add`, (req, res) => res.render(`pages/post`));
+articlesRoutes.get(`/edit/:id`, (req, res) => res.render(`pages/post`));
+articlesRoutes.get(`/:id`, (req, res) => res.render(`pages/post-detail`));
+articlesRoutes.get(`/category/:id`, (req, res) => res.render(`pages/articles-by-category`));
 
 module.exports = articlesRoutes;
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -4,8 +4,8 @@ const {Router} = require(`express`);
 
 const myRoutes = new Router();
 
-myRoutes.get(`/`, (req, res) => res.send(`/my`));
-myRoutes.get(`/categories`, (req, res) => res.send(`/my/categories`));
-myRoutes.get(`/comments`, (req, res) => res.send(`/my/comments`));
+myRoutes.get(`/`, (req, res) => res.render(`pages/my`));
+myRoutes.get(`/categories`, (req, res) => res.render(`pages/all-categories`));
+myRoutes.get(`/comments`, (req, res) => res.render(`pages/comments`));
 
 module.exports = myRoutes;
