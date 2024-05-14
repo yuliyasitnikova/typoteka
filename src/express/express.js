b61e9d21c164fecdd2eda7b85db78b0569104bc1
Юлия Ситникова <yu.sitnikova@inbox.ru>
2024-05-15 00:38:45 +0500

    Добавляет папку со шаблонами

--- src/express/express.js
+++ src/express/express.js
@@ -8,6 +8,7 @@ const mainRoutes = require(`./routes/main-routes`);
 
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
+const VIEWS_DIR = `templates`;
 
 const app = express();
 
@@ -16,5 +17,11 @@ app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
+
+app.use((req, res) => res.status(400).render(`pages/404`));
+app.use((err, req, res, _next) => res.status(500).render(`pages/500`));
+
+app.set(`views`, path.resolve(__dirname, VIEWS_DIR));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
