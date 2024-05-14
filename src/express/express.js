9ec0fe1ea517db101c1869365b6e605a882cb24c
Юлия Ситникова <yu.sitnikova@inbox.ru>
2024-05-14 22:56:17 +0500

    Добавляет папку со статичными ресурсами

--- src/express/express.js
+++ src/express/express.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
